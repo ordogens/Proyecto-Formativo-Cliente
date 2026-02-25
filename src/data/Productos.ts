34d4e8d4b3a3cd187df3be4eb4c1db6485cd6869
ordogen <oarcilagarcia@gmail.com>
2026-02-24 22:01:33 -0500

    creaacion basica de los modulos del ecommerce

--- src/data/Productos.ts
+++ src/data/Productos.ts
@@ -1,4 +1,6 @@
 import camisa from "../assets/photo.png";
+import mujer from "../assets/mujer.jpg";
+import gorro from "../assets/gorros.jpg";
 
 
 export interface Producto {
@@ -40,5 +42,75 @@ export const productos: Producto[] = [
     precio: 10000,
     imagen: camisa,
   },
+
+];
+
+export const productoM: Producto[] = [
+
+  {
+    id: 1,
+    nombre: "Nombre producto",
+    precio: 10000,
+    imagen: mujer,
+  },
+  {
+    id: 2,
+    nombre: "Nombre producto",
+    precio: 10000,
+    imagen: mujer,
+  },
+  {
+    id: 3,
+    nombre: "Nombre producto",
+    precio: 10000,
+    imagen: mujer,
+  },
+  {
+    id: 4,
+    nombre: "Nombre producto",
+    precio: 10000,
+    imagen: mujer,
+  },
+   {
+    id: 5,
+    nombre: "Nombre producto",
+    precio: 10000,
+    imagen: mujer,
+   },
+   
+];
+
+export const gorros: Producto[] = [
+
+  {
+    id: 1,
+    nombre: "Nombre producto",
+    precio: 10000,
+    imagen: gorro,
+  },
+  {
+    id: 2,
+    nombre: "Nombre producto",
+    precio: 10000,
+    imagen: gorro,
+  },
+  {
+    id: 3,
+    nombre: "Nombre producto",
+    precio: 10000,
+    imagen: gorro,
+  },
+  {
+    id: 4,
+    nombre: "Nombre producto",
+    precio: 10000,
+    imagen: gorro,
+  },
+   {
+    id: 5,
+    nombre: "Nombre producto",
+    precio: 10000,
+    imagen: gorro,
+   },
 
 ];
--- src/pages/RMujer/RopaMujer.tsx
+++ src/pages/RMujer/RopaMujer.tsx
@@ -1,13 +1,37 @@
-
-
+import { ProductCard } from "../../components/cards/ProductCard";
+import { productoM } from "../../data/Productos";
 
 export const RopaMujer = () => {
   return (
-     <section className="bg-[#f3f0eb] text-black h-full ">
-      <h1>ropa mujer</h1>
-      
+    <section className="h-full bg-[#f3f0eb] py-12">
+      {/* Header */}
+      <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-6 md:px-16">
+        <div>
+          <p className="text-red-400 font-medium">Categorías</p>
+          <h1 className="text-3xl md:text-4xl font-serif font-semibold">
+            Ropa de Mujer
+          </h1>
+          <p className="text-gray-500 text-sm mt-2">
+            {productoM.length} productos disponibles
+          </p>
+        </div>
+
+        <div className="flex gap-3 ">
+          <button className="border px-4 py-2 rounded-lg text-sm hover:bg-gray-200 transition">
+            Camisas
+          </button>
+          <button className="border px-4 py-2 rounded-lg text-sm hover:bg-gray-200 transition">
+            Pantalones
+          </button>
+        </div>
+      </div>
+
+      {/* Grid */}
+      <div className="mt-12 grid grid-cols-2 gap-6 sm:grid-cols-2 lg:grid-cols-5  w-screen md:px-10">
+        {productoM.map((producto) => (
+          <ProductCard key={producto.id} producto={producto} />
+        ))}
+      </div>
     </section>
-  )
-}
-
-
+  );
+};
